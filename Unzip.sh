da3cd37ce3dfd80c062ce7db84d1d09d5121668f
Anton M <anton.malkovski@gmail.com>
2023-12-18 18:44:12 +0200

    Unzip time upd

--- Unzip.sh
+++ Unzip.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=unzip_job
-#SBATCH --time=00:30:00               # Adjust as needed (hours:minutes:seconds)
+#SBATCH --time=01:30:00               # Adjust as needed (hours:minutes:seconds)
 #SBATCH --output=unzip_output.log
 #SBATCH --error=unzip_error.lo
 
