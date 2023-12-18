067a1d48f5a2a08c032aa29f780f317ca714e5d5
Anton M <anton.malkovski@gmail.com>
2023-12-19 00:28:50 +0200

    new py

--- augmentor.sh
+++ augmentor.sh
@@ -9,7 +9,5 @@
 
 cd /gpfs/space/home/amlk/data
 
-module load python/3.9.12
-module load py-pandas/1.1.4
-module load py-numpy/1.19.2
-srun python3.9.12 augmentor.py
+module load python/3.8.3
+srun python3.8.3 augmentor.py
