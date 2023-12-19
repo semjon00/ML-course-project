df4ab726df60bbeeadabec674e25e9b0e3b54ff0
Anton M <anton.malkovski@gmail.com>
2023-12-19 02:41:49 +0200

    upd

--- augmentor.sh
+++ augmentor.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 #SBATCH --job-name=image_augmentation
 #SBATCH --time=06:30:00        
-#SBATCH --output=unzip_output.log
-#SBATCH --error=unzip_error.log
+#SBATCH --output=output.log
+#SBATCH --error=error.log
 #SBATCH--mail-user=anton.malkovski@gmail.com
 #SBATCH--mail-type=ALL
+#SBATCH --partition=intel
 
 
 cd /gpfs/space/home/amlk/data
 
-module load python/3.8.3
-srun python3.8.3 augmentor.py
+module load python
+
+srun python augmentor.py
